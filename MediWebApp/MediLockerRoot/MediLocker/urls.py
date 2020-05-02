8d6a7d198bda336d15e976601e1724bfda5c7a2c
rahulsunil2 <rahulsunil2@gmail.com>
2020-05-03 02:07:01 +0530

    Added Rest API for Authentication

--- MediWebApp/MediLockerRoot/MediLocker/urls.py
+++ MediWebApp/MediLockerRoot/MediLocker/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="social_app/index.html")), # <--
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # <--
-    path('api/', include('MediLocker.urls', namespace='api')),
+    path('api/', include('MediLockerApp.urls', namespace='MediLockerApp')),
     path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
--- MediWebApp/MediLockerRoot/MediLockerApp/urls.py
+++ MediWebApp/MediLockerRoot/MediLockerApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import UserRecordView
 
-app_name = 'api'
+app_name = 'MediLockerApp'
 urlpatterns = [
     path('user/', UserRecordView.as_view(), name='users'),
 ]
