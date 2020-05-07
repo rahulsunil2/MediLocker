1e6d844f20be2b01721d256aeb46cf6742454d0b
Kesia Mary Joies <kesiajoies@gmail.com>
2020-05-08 00:01:34 +0400

    Login with django

---
+++ flutter/med_report/lib/repository/user_repository.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:med_report/bloc_login/api_connection/api_connection.dart';
+import 'package:med_report/bloc_login/dao/user_dao.dart';
+import 'package:med_report/bloc_login/model/api_model.dart';
+import 'package:med_report/bloc_login/model/user_model.dart';
+import 'package:meta/meta.dart';
+
+
+class UserRepository {
+  final userDao = UserDao();
+
+  Future<User> authenticate ({
+    @required String username,
+    @required String password,
+  }) async {
+    UserLogin userLogin = UserLogin(
+      username: username,
+      password: password
+    );
+    Token token = await getToken(userLogin);
+    User user = User(
+      id: 0,
+      username: username,
+      token: token.token,
+    );
+    return user;
+  }
+
+  Future<void> persistToken ({
+    @required User user
+    }) async {
+    // write token with the user to the database
+      await userDao.createUser(user);
+  }
+
+  Future <void> delteToken({
+    @required int id
+  }) async {
+    await userDao.deleteUser(id);
+  }
+
+  Future <bool> hasToken() async {
+    bool result = await userDao.checkUser(0);
+    return result;
+  }
+}
