385ba3eda66e24c5a82ae1e63caa2427f8f10666
abhijeet1999 <38234788+abhijeet1999@users.noreply.github.com>
2020-07-16 12:37:38 +0530

    added other profile

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,13 @@
 class Common {
   static String currentUser = 'mediLocker';
   static String gender;
-  static String blood;
   static String baseURL = 'http://134.209.158.239:8000/';
+  static String user;
+  static String phone;
+  static  String dob;
+  static  String address;
+  static  String allergy;
+  static String blood_grp;
+  static String height;
+  static  String weight;
 }
