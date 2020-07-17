149fa1ed33865aaec4eb9faabd6da9efcacc8bd2
abhijeet1999 <38234788+abhijeet1999@users.noreply.github.com>
2020-07-17 19:22:31 +0530

    profile ui

--- lib/global.dart
+++ lib/global.dart
@@ -1,13 +1,13 @@
 class Common {
   static String currentUser = 'mediLocker';
-  static String gender;
+  static String gender = '';
   static String baseURL = 'http://134.209.158.239:8000/';
   static String user;
-  static String phone;
-  static  String dob;
-  static  String address;
-  static  String allergy;
-  static String blood_grp;
-  static String height;
-  static  String weight;
+  static String phone= 'None';
+  static  String dob= 'NIL';
+  static  String address= 'NIL';
+  static  String allergy='NIL';
+  static String blood_grp='NIL';
+  static String height='NIL';
+  static  String weight='NIL';
 }
