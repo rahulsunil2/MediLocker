452b53bdc4ea0b2d1bb1db0c105cda7b8ae88cd8
Rahul Sunil <rahulsunil2@gmail.com>
2020-07-21 06:15:08 +0530

    > Admin View Changed

--- MediWebApp/MediCare/MediCareApp/admin.py
+++ MediWebApp/MediCare/MediCareApp/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 from .models import MedicalImageFile, UserProfile, UserMedicalData
 
 # Register your models here.
 
-admin.site.register(MedicalImageFile)
-admin.site.register(UserProfile)
-admin.site.register(UserMedicalData)
+
+@register(MedicalImageFile)
+class MedicalImageAdmin(admin.ModelAdmin):
+    list_display =['file', 'description', 'user', 'extraction_status', 'type', 'category', 'record_date', 'uploaded_at']
+
+
+@register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'dob', 'address', 'allergy', 'gender', 'blood_grp', 'height', 'weight']
+
+
+@register(UserMedicalData)
+class MedicalDataAdmin(admin.ModelAdmin):
+    list_display = ['user', 'original_file', 'extracted_data', 'uploaded_time']
