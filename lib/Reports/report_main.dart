daa09c129eb63345de08c0e5423f40fb466a4277
Akanksha Singh <akankshasingh121297@gmail.com>
2020-07-22 15:16:37 +0530

    Added Slider for graphs

--- lib/Reports/report_main.dart
+++ lib/Reports/report_main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:med_report/Reports/files_categories.dart';
 import 'package:med_report/Reports/upload.dart';
 import 'package:med_report/Reports/graph.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Options extends StatelessWidget {
   @override
@@ -49,7 +50,7 @@ class Options extends StatelessWidget {
         body: TabBarView(
           children: [
             Category(),
-            DateTimeChart.withSampleData(),
+            Swiper(),
             AddReport()
             //selectLoc(context),
             //form(context),
@@ -57,5 +58,109 @@ class Options extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Swiper extends StatefulWidget {
+  @override
+  _SwiperState createState() => _SwiperState();
+}
+
+class _SwiperState extends State<Swiper> {
+  int _currentIndex=0;
+
+  List cardList=[
+    Item1(),
+    Item2(),
+    Item3(),
+    Item4()
+  ];
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: <Widget>[
+            CarouselSlider(
+               options: CarouselOptions(height: MediaQuery.of(context).size.height/1.3,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              aspectRatio: 2.0,),
+              items: cardList.map((card){
+                return Builder(
+                  builder:(BuildContext context){
+                    return Container(
+                      height: 400,
+                      width: MediaQuery.of(context).size.width,
+                      child: Card(
+                        color: Colors.blueAccent,
+                        child: card,
+                      ),
+                    );
+                  }
+                );
+              }).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: map<Widget>(cardList, (index, url) {
+                return Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                  ),
+                );
+              }),
+            ),
+          ],
+        )
+      );
+  }
+}
+
+class Item1 extends StatelessWidget {
+  const Item1({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DateTimeChart.withSampleData();
+  }
+}
+
+class Item2 extends StatelessWidget {
+  const Item2({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DateTimeChart.withSampleData();
+  }
+}
+
+class Item3 extends StatelessWidget {
+  const Item3({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DateTimeChart.withSampleData();
+  }
+}
+
+class Item4 extends StatelessWidget {
+  const Item4({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return DateTimeChart.withSampleData();
   }
 }
