41850de2a6656b25998c82017ed4b6529d4f5209
Rahul Sunil <rahulsunil2@gmail.com>
2020-08-26 12:59:31 +0530

    Media Root to url

--- MediWebApp/MediCare/MediCare/urls.py
+++ MediWebApp/MediCare/MediCare/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.authtoken import views as rest_views
 
+
 urlpatterns = [
+    path('', include('MediCareApp.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('MediCareApp.urls')),
     path('api-token-auth/', rest_views.obtain_auth_token, name='api-token-auth'),
