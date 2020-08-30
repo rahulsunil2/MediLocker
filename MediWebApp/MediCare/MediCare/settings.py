26843d86fd00189c27740247d1071175758af6dd
AWS <AWS@rahul.com>
2020-08-30 17:16:33 +0000

    Deployed Version

--- MediWebApp/MediCare/MediCare/settings.py
+++ MediWebApp/MediCare/MediCare/settings.py
@@ -129,3 +129,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- MediWebApp/MediCare/MediCare/urls.py
+++ MediWebApp/MediCare/MediCare/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.authtoken import views as rest_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('MediCareApp.urls')),
     path('api-token-auth/', rest_views.obtain_auth_token, name='api-token-auth'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
